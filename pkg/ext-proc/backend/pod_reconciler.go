d5d6c7e7b652d4b7a068d4835b26737bcc74ad39
Ondřej Kupka <ondra.cap@gmail.com>
2025-02-15 19:55:50 +0100

    pod_reconciler: Use TRACE log level
    Co-authored-by: Abdullah Gharaibeh <40361897+ahg-g@users.noreply.github.com>

--- pkg/ext-proc/backend/pod_reconciler.go
+++ pkg/ext-proc/backend/pod_reconciler.go
@@ -26,7 +26,7 @@ func (c *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	logger := log.FromContext(ctx)
 	inferencePool, err := c.Datastore.getInferencePool()
 	if err != nil {
-		logger.V(logutil.DEFAULT).Info("Skipping reconciling Pod because the InferencePool is not available yet", "error", err)
+		logger.V(logutil.TRACE).Info("Skipping reconciling Pod because the InferencePool is not available yet", "error", err)
 		// When the inferencePool is initialized it lists the appropriate pods and populates the datastore, so no need to requeue.
 		return ctrl.Result{}, nil
 	} else if inferencePool.Namespace != req.Namespace {
